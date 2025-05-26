99e3151b65f8ce166afeeaa400e9ebce277dacca
RoshiniMadhaviLathaBoddu <roshinimadhavi.boddu@gmail.com>
2025-05-26 16:44:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,10 @@ const cors = require('cors');
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  credentials: true,
+}));
 app.use(express.json());
 
 const userRoutes = require('./routes/userRoutes');
